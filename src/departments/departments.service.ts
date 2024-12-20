b6d440057a7745790eec6973d6176f6e6be79486
Omogo Peter <155792462+onyedika-glitch@users.noreply.github.com>
2024-12-20 01:53:14 +0100

    Update departments.service.ts

--- src/departments/departments.service.ts
+++ src/departments/departments.service.ts
@@ -1,18 +1,42 @@
-import { Injectable } from '@nestjs/common';
+// src/department/department.service.ts
+
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Department } from './departments.entity';
-import { CreateDepartmentDto } from './create-departments.dto';
+import { Department } from './department.entity';
+import { SubDepartment } from './sub-department.entity';
+import { CreateDepartmentInput } from './dto/create-department.input';
+import { UpdateDepartmentInput } from './dto/update-department.input';
 
 @Injectable()
-export class DepartmentsService {
+export class DepartmentService {
   constructor(
     @InjectRepository(Department)
     private departmentRepository: Repository<Department>,
+    @InjectRepository(SubDepartment)
+    private subDepartmentRepository: Repository<SubDepartment>,
   ) {}
 
-  async create(createDepartmentDto: CreateDepartmentDto): Promise<Department> {
-    const department = this.departmentRepository.create(createDepartmentDto);
+  async create(input: CreateDepartmentInput): Promise<Department> {
+    const { name, subDepartments } = input;
+
+    const department = this.departmentRepository.create({
+      name,
+      subDepartments: subDepartments ? subDepartments.map(subDept => this.subDepartmentRepository.create(subDept)) : [],
+    });
+
+    return this.departmentRepository.save(department);
+  }
+
+  async update(input: UpdateDepartmentInput): Promise<Department> {
+    const { id, name } = input;
+    const department = await this.departmentRepository.findOne(id);
+
+    if (!department) {
+      throw new NotFoundException('Department not found');
+    }
+
+    department.name = name;
     return this.departmentRepository.save(department);
   }
 }
